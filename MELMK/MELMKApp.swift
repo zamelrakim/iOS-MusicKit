63847b97079b0ac0be7253a4c4ce6f7b3030b5d7
Zamel Rakim <zrhill@gmail.com>
2020-10-08 17:04:19 -0400

    Initial Commit

---
+++ MELMK/MELMKApp.swift
@@ -0,0 +1,17 @@
+//
+//  MELMKApp.swift
+//  MELMK
+//
+//  Created by Zamel Rakim on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct MELMKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
